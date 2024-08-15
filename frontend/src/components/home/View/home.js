a59f7987e7623ebcfe95568553ab6787ebf0221b
ayoub aitouna <aitounaayoub05@gmail.com>
2024-08-15 18:12:31 +0100

    implemented home ui skeletion and fixed some css bugs in navigation bar

--- frontend/src/components/home/View/home.js
+++ frontend/src/components/home/View/home.js
@@ -1,9 +1,33 @@
 export default function() {
     return /*html*/`
-		<div class= "auth">
-		<div class="auth-container">
-			<h1>HERE Home Page</>
-		</div>
+    <div class="home">
+      <div class="home-container">
+        <div class="grid-container">
+          <div class="item-section item-a">
+            TeamLeader
+          </div>
+          <div class="item-section item-b">
+            NewTournaments
+          </div>
+          <div class="item-section item-c">
+            TopPlayers 
+          </div>
+        </div>
+
+        <div class="grid-container">
+          <div class="item-a h-[456px] item-section">
+            OnlinePlayers
+          </div>
+          <div
+            class="item-b h-[456px] item-section"
+          >
+            Tournaments
+          </div>
+          <div class="item-c h-[456px] item-section">
+            Friends
+          </div>
+        </div>
+      </div>
     </div>
     `;
 };
--- frontend/src/components/navBar/Controller/navBar.js
+++ frontend/src/components/navBar/Controller/navBar.js
@@ -1,108 +1,110 @@
-
 const notifications = [
-	{
-		text: "You have a new message",
-		time: "10:24",
-		image: "components/auth/assets/google.svg"
-	},
-	{
-		text: "You have a new message",
-		time: "10:24",
-		image: "components/auth/assets/google.svg"
-	},
-	{
-		text: "You have a new message",
-		time: "10:24",
-		image: "components/auth/assets/google.svg"
-	},
-	{
-		text: "You have a new message",
-		time: "10:24",
-		image: "components/auth/assets/google.svg"
-	},
-	{
-		text: "You have a new message",
-		time: "10:24",
-		image: "components/auth/assets/google.svg"
-	},
-	{
-		text: "You have a new message",
-		time: "10:24",
-		image: "components/auth/assets/google.svg"
-	},
-	{
-		text: "You have a new message",
-		time: "10:24",
-		image: "components/auth/assets/google.svg"
-	},
-	{
-		text: "Your order has been shipped",
-		time: "11:00",
-		image: "/components/auth/assets/42_logo.svg"
-	}
+  {
+    text: "You have a new message",
+    time: "10:24",
+    image: "components/auth/assets/google.svg",
+  },
+  {
+    text: "You have a new message",
+    time: "10:24",
+    image: "components/auth/assets/google.svg",
+  },
+  {
+    text: "You have a new message",
+    time: "10:24",
+    image: "components/auth/assets/google.svg",
+  },
+  {
+    text: "You have a new message",
+    time: "10:24",
+    image: "components/auth/assets/google.svg",
+  },
+  {
+    text: "You have a new message",
+    time: "10:24",
+    image: "components/auth/assets/google.svg",
+  },
+  {
+    text: "You have a new message",
+    time: "10:24",
+    image: "components/auth/assets/google.svg",
+  },
+  {
+    text: "You have a new message",
+    time: "10:24",
+    image: "components/auth/assets/google.svg",
+  },
+  {
+    text: "Your order has been shipped",
+    time: "11:00",
+    image: "/components/auth/assets/42_logo.svg",
+  },
 ];
 
 function getCookieValue(name) {
-	const cookies = document.cookie.split(';');
-	for (let i = 0; i < cookies.length; i++) {
-		const cookie = cookies[i].trim();
-		if (cookie.startsWith(name + '=')) {
-			return cookie.substring(name.length + 1);
-		}
-	}
-	return null;
+  const cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(name + "=")) {
+      return cookie.substring(name.length + 1);
+    }
+  }
+  return null;
 }
 
-
 async function loadNavbar() {
-	const path = window.location.pathname;
+  const path = window.location.pathname;
 
-	if (!path.startsWith('/game') && !path.startsWith('/sign')) {
-		try {
-			const response = await fetch('/components/navBar/View/navBar.html');
-			if (!response.ok) throw new Error('Network response was not ok');
-			const navbarHTML = await response.text();
+  if (!path.startsWith("/game") && !path.startsWith("/sign")) {
+    try {
+      const response = await fetch("/components/navBar/View/navBar.html");
+      if (!response.ok) throw new Error("Network response was not ok");
+      const navbarHTML = await response.text();
 
-			document.body.insertAdjacentHTML('afterbegin', navbarHTML);
+      document.body.insertAdjacentHTML("afterbegin", navbarHTML);
 
-			renderNavBrContent();
+      renderNavBrContent();
 
-			fetchMyData();
-			renderNotifications();
+      fetchMyData();
+      renderNotifications();
 
-			document.getElementById('notif').addEventListener('click', function () {
-				const panel = document.getElementById('notification-panel');
-				panel.classList.toggle('hidden');
-			});
+      document.getElementById("notif").addEventListener("click", function () {
+        const panel = document.getElementById("notification-panel");
+        panel.classList.toggle("hidden");
+      });
 
-			document.addEventListener('DOMContentLoaded', renderNotifications);
+      document.addEventListener("DOMContentLoaded", renderNotifications);
 
-			document.addEventListener('click', function (event) {
-				const panel = document.getElementById('notification-panel');
-				const notifButton = document.getElementById('notif');
-				if (!panel.contains(event.target) && !notifButton.contains(event.target)) {
-					panel.classList.add('hidden');
-				}
-			});
-
-		} catch (error) {
-			console.error('Error loading navbar:', error);
-		}
-	}
+      document.addEventListener("click", function (event) {
+        const panel = document.getElementById("notification-panel");
+        const notifButton = document.getElementById("notif");
+        if (
+          !panel.contains(event.target) &&
+          !notifButton.contains(event.target)
+        ) {
+          panel.classList.add("hidden");
+        }
+      });
+    } catch (error) {
+      console.error("Error loading navbar:", error);
+    }
+  }
 }
 
 function renderNotifications() {
-	const notificationList = document.getElementById('notification-list');
-	notificationList.innerHTML = '';
+  const notificationList = document.getElementById("notification-list");
+  notificationList.innerHTML = "";
 
-	notifications.forEach(notification => {
-		const notificationItem = document.createElement('div');
-		notificationItem.className = 'notification-item';
+  notifications.forEach((notification) => {
+    const notificationItem = document.createElement("li");
+    notificationItem.className = "notification-item";
 
-		notificationItem.innerHTML = `
-            <a href="${notification.link}" class="notification-link">
+    notificationItem.innerHTML = /*html*/ `
+            <a href="${notification.link || ""}" class="notification-link">
                 <div class="notification-image-container">
-                    <img class="notification-image" src="${notification.image}" alt="Profile Image" width="35" height="35" />
+                    <img class="notification-image" src="${
+                      notification.image
+                    }" alt="Profile Image" width="35" height="35" />
                 </div>
                 <div class="notification-text-container">
                     <div class="notification-text">${notification.text}</div>
@@ -110,88 +112,93 @@ function renderNotifications() {
                 </div>
             </a>
             <div class="notification-menu-container">
-                <div class="notification-menu">
-                    <div class="three-points-trigger">
-						<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M12 6.42C12.2546 6.42 12.4988 6.31886 12.6789 6.13882C12.8589 5.95879 12.96 5.71461 12.96 5.46C12.96 5.20539 12.8589 4.96121 12.6789 4.78118C12.4988 4.60114 12.2546 4.5 12 4.5C11.7454 4.5 11.5013 4.60114 11.3212 4.78118C11.1412 4.96121 11.04 5.20539 11.04 5.46C11.04 5.71461 11.1412 5.95879 11.3212 6.13882C11.5013 6.31886 11.7454 6.42 12 6.42ZM12 13.46C12.2546 13.46 12.4988 13.3589 12.6789 13.1788C12.8589 12.9988 12.96 12.7546 12.96 12.5C12.96 12.2454 12.8589 12.0012 12.6789 11.8212C12.4988 11.6411 12.2546 11.54 12 11.54C11.7454 11.54 11.5013 11.6411 11.3212 11.8212C11.1412 12.0012 11.04 12.2454 11.04 12.5C11.04 12.7546 11.1412 12.9988 11.3212 13.1788C11.5013 13.3589 11.7454 13.46 12 13.46ZM12 20.5C12.2546 20.5 12.4988 20.3989 12.6789 20.2188C12.8589 20.0388 12.96 19.7946 12.96 19.54C12.96 19.2854 12.8589 19.0412 12.6789 18.8612C12.4988 18.6811 12.2546 18.58 12 18.58C11.7454 18.58 11.5013 18.6811 11.3212 18.8612C11.1412 19.0412 11.04 19.2854 11.04 19.54C11.04 19.7946 11.1412 20.0388 11.3212 20.2188C11.5013 20.3989 11.7454 20.5 12 20.5Z" stroke="#F8F8F8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-						</svg>
-					</div>
-                </div>
+				<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M7.97471 11.9906H12.5147C12.6915 12.0006 12.8679 11.965 13.0271 11.8874C13.1862 11.8098 13.3228 11.6927 13.4239 11.5473C13.525 11.4019 13.5871 11.2331 13.6044 11.0569C13.6217 10.8806 13.5936 10.7029 13.5227 10.5406C13.2827 9.81532 12.326 8.94532 12.326 8.08932C12.326 6.18932 12.326 5.68932 11.3907 4.57198C11.0876 4.21226 10.7118 3.92077 10.288 3.71665L9.76605 3.46332C9.67846 3.41045 9.6029 3.33984 9.54423 3.25602C9.48556 3.17221 9.44507 3.07704 9.42538 2.97665C9.3732 2.63825 9.19427 2.33237 8.92488 2.12104C8.65549 1.90971 8.3158 1.80876 7.97471 1.83865C7.63982 1.81869 7.3095 1.9242 7.04819 2.13461C6.78689 2.34501 6.61333 2.64521 6.56138 2.97665C6.53795 3.08045 6.49202 3.17784 6.42681 3.26193C6.36161 3.34602 6.27874 3.41477 6.18405 3.46332L5.66138 3.71665C5.23807 3.92092 4.86275 4.21241 4.56005 4.57198C3.62471 5.68932 3.62471 6.18932 3.62471 8.08932C3.62471 8.94532 2.71138 9.72798 2.47138 10.4966C2.32671 10.9613 2.24671 11.9906 3.45805 11.9906H7.97471Z" stroke="#A2A2A2" stroke-linecap="round" stroke-linejoin="round"/>
+					<path d="M10.1507 11.9907C10.1568 12.2781 10.1047 12.5638 9.99748 12.8305C9.89028 13.0972 9.73021 13.3395 9.52691 13.5427C9.32361 13.746 9.0813 13.906 8.81454 14.0131C8.54779 14.1202 8.26211 14.1722 7.97472 14.1661C7.68741 14.1721 7.40184 14.12 7.1352 14.0129C6.86856 13.9057 6.62636 13.7457 6.42319 13.5424C6.22002 13.3392 6.06007 13.097 5.95298 12.8303C5.84589 12.5636 5.79387 12.278 5.80005 11.9907" stroke="#A2A2A2" stroke-linecap="round" stroke-linejoin="round"/>
+					<path d="M9.16671 6.3313L6.83337 8.6593M6.83337 6.33597L9.16671 8.66463" stroke="#A2A2A2" stroke-miterlimit="10" stroke-linecap="round"/>
+				</svg>
             </div>
         `;
 
-		notificationList.appendChild(notificationItem);
-	});
+    notificationList.appendChild(notificationItem);
+  });
 }
 
-
 function renderNavBrContent() {
+  console.log("Applying navbar styles...");
+  const navItems = document.querySelectorAll(".nav-item.nav a");
 
+  navItems.forEach((navItem) => {
+    if (window.location.pathname === navItem.getAttribute("href")) {
+      const svg = navItem.querySelector("svg path");
+      const paragraph = navItem.querySelector("p");
 
-	console.log("Applying navbar styles...");
-	const navItems = document.querySelectorAll('.nav-item.nav a');
+      if (svg) svg.setAttribute("fill", "#FD4106");
+      if (paragraph) paragraph.style.color = "#FD4106";
+      const underline = document.createElement("div");
+      underline.style.width = "100%";
+      underline.style.height = "2px";
+      underline.style.backgroundColor = "#FD4106";
+      underline.style.position = "absolute";
+      underline.style.bottom = "0";
+      underline.style.left = "0";
+      navItem.style.position = "relative";
 
-	navItems.forEach(navItem => {
-		if (window.location.pathname === navItem.getAttribute('href')) {
-			const svg = navItem.querySelector('svg path');
-			const paragraph = navItem.querySelector('p');
-
-			if (svg) svg.setAttribute('fill', '#FD4106');
-			if (paragraph) paragraph.style.color = '#FD4106';
-			const underline = document.createElement('div');
-			underline.style.width = '100%';
-			underline.style.height = '2px';
-			underline.style.backgroundColor = '#FD4106';
-			underline.style.position = 'absolute';
-			underline.style.bottom = '0';
-			underline.style.left = '0';
-			navItem.style.position = 'relative';
-
-			navItem.appendChild(underline);
-		}
-	});
+      navItem.appendChild(underline);
+    }
+  });
 }
 
 async function fetchMyData() {
-	const accessToken = getCookieValue('access');
-	const refreshToken = getCookieValue('refresh'); // Note: This might not be needed for this request
+  const accessToken = getCookieValue("access");
+  const refreshToken = getCookieValue("refresh"); // Note: This might not be needed for this request
 
-	if (!accessToken || refreshToken) return;
-	const apiUrl = 'https://localhost:4433/api/v1/users/me/';
+  if (!accessToken || refreshToken) return;
+  const apiUrl = "https://localhost:4433/api/v1/users/me/";
 
-	try {
-		const response = await fetch(apiUrl, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': `Bearer ${accessToken}` // Include the access token in the Authorization header
-			}
-		});
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`);
-		}
-		const data = await response.json();
-		data.image_url = data.image_url.replace('https://localhost/', 'https://localhost:4433/');
-		data.rank.icon = data.rank.icon.replace('https://localhost/', 'https://localhost:4433/');
-		handleUserData(data);
-	} catch (error) {
-		console.error('Error fetching user data:', error);
-	}
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`, // Include the access token in the Authorization header
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    data.image_url = data.image_url.replace(
+      "https://localhost/",
+      "https://localhost:4433/"
+    );
+    data.rank.icon = data.rank.icon.replace(
+      "https://localhost/",
+      "https://localhost:4433/"
+    );
+    handleUserData(data);
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+  }
 }
 
 function handleUserData(data) {
-	const profileButton = document.querySelector('.profile-button .profile');
-	console.log(data.username);
-	if (!profileButton) return;
+  const profileButton = document.querySelector(".profile-button .profile");
+  console.log(data.username);
+  if (!profileButton) return;
 
-	profileButton.innerHTML = `
+  profileButton.innerHTML = `
 		<div class="profile-content">
 			<div class="profile-image">
-				<img src="${data.image_url || 'components/auth/assets/google.svg'}" alt="Profile Image" />
+				<img src="${
+          data.image_url || "components/auth/assets/google.svg"
+        }" alt="Profile Image" />
 			</div>
 			<div class="profile-details">
 				<div class="profile-name">${data.username}</div>
-				<div class="profile-level">Level ${data.current_xp + "/" + data.rank.xp_required}</div>
+				<div class="profile-level">Level ${
+          data.current_xp + "/" + data.rank.xp_required
+        }</div>
 			</div>
 		</div>
 		<div id="profile-icon" class="profile-dropdown-icon">
@@ -203,8 +210,8 @@ function handleUserData(data) {
 }
 
 // Ensure DOM is fully loaded before calling fetchMyData
-document.addEventListener('DOMContentLoaded', function () {
-	fetchMyData();
+document.addEventListener("DOMContentLoaded", function () {
+  fetchMyData();
 });
 
 loadNavbar();
