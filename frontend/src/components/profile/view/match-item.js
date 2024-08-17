8a8018d82980b30e742d707e560ed24e5428148f
ayoub aitouna <aitounaayoub05@gmail.com>
2024-08-17 22:03:52 +0100

    added profile page and working on it's controller

---
+++ frontend/src/components/profile/view/match-item.js
@@ -0,0 +1,59 @@
+const html = String.raw;
+
+export function matchItem({ first_player, second_player }) {
+  return html`
+    <a href="/">
+      <div class="match-item">
+        <div class="match-players">
+          <div class="player-icons">
+            <div class="player-icon">
+              <img
+                src="${first_player.icon ||
+                "https://images.unsplash.com/photo-1667053508464-eb11b394df83?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}"
+                alt="profile"
+              />
+            </div>
+            <div class="player-icon">
+              <img
+                src="${second_player.icon ||
+                "https://images.unsplash.com/photo-1667053508464-eb11b394df83?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}"
+                alt="profile"
+              />
+            </div>
+          </div>
+          <div class="player-details">
+            <div class="player-name" title="${first_player.name}">
+              ${first_player.name}
+            </div>
+            <div class="player-level">
+              Level <span>${first_player.level}</span>
+            </div>
+          </div>
+          <p class="vs-text">VS</p>
+          <div class="player-details">
+            <div class="player-name" title="${second_player.name}">
+              ${second_player.name}
+            </div>
+            <div class="player-level">
+              Level <span>${second_player.level}</span>
+            </div>
+          </div>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+        >
+          <path
+            stroke="#fff"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="{1.5}"
+            d="m9 5.405 7 7-7 7"
+          />
+        </svg>
+      </div>
+    </a>
+  `;
+}
--- frontend/src/routesConfig.js
+++ frontend/src/routesConfig.js
@@ -1,19 +1,96 @@
 const routes = {
-	"/": { title: "Home", component: () => import("./components/home/View/home.js"), css: './components/home/assets/home.css', controller: () => import('./components/home/Controller/home.js') },
-	"/chat": { title: "Chat", component: () => import("./components/chat/View/chat.js"), css: './components/chat/assets/chat.css', controller: () => import('./components/chat/Controller/chat.js') },
-	"/tournaments": { title: "Tournaments", component: () => import("./components/tournaments/View/Tournaments.js"), css: './components/tournaments/assets/Tournaments.css', controller: () => import('./components/tournaments/Controller/Tournaments.js') },
-	"/tournaments/tournament": { title: "Tournament", component: () => import("/components/tournament/View/Tournament.js"), css: '/components/tournament/assets/Tournament.css', controller: () => import('/components/tournament/Controller/Tournament.js') },
-	"/ranking": { title: "Ranking", component: () => import("./components/Ranking/View/Ranking.js"), css: "./components/Ranking/assets/Ranking.css", controller: () => import('./components/Ranking/Controller/Ranking.js') },
-	"/sign_in": { title: "Sign In", component: () => import("./components/auth/View/sing_in.js"), css: './components/auth/assets/auth.css', controller: () => import('./components/auth/Controller/sing_in.js') },
-	"/sign_in_2fa": { title: "Two-Factor Authentication", component: () => import("./components/auth/View/totp.js"), css: './components/auth/assets/auth.css', controller: () => import('./components/auth/Controller/totp.js') },
-	"/sign_up": { title: "Sign Up", component: () => import("./components/auth/View/sign_up.js"), css: './components/auth/assets/auth.css', controller: () => import('./components/auth/Controller/sign_up.js') },
-	"/sign_up_email": { title: "Email Verification", component: () => import("./components/auth/View/email_ver.js"), css: './components/auth/assets/auth.css', controller: () => import('./components/auth/Controller/email_ver.js') },
-	"/sign_up_info": { title: "Your Information", component: () => import("./components/auth/View/use_info.js"), css: './components/auth/assets/auth.css', controller: () => import('./components/auth/Controller/use_info.js') },
-	"/game_match_making": { title: "Match Making", component: () => import("./components/game/match_making/View/match_making.js"), css: './components/game/match_making/assets/match_making.css', controller: () => import('./components/game/match_making/Controller/match_making.js') },
-	"/game": { title: "In Game", component: () => import("./components/game/in_game/View/game.js"), css: './components/game/in_game/assets/game.css', controller: () => import('./components/game/in_game/Controller/game.js') },
-	"/settings": { title: "Settings", component: () => import("./components/settings/View/Settings.js"), css: './components/settings/assets/style.css', controller: () => import('./components/settings/Controller/Settings.js') },
-	"*": { title: "404 Not Found", component: () => import('./lib/NotFound/NotFound.js'), css: './lib/NotFound/notFound.css' },
-
+  "/": {
+    title: "Home",
+    component: () => import("./components/home/View/home.js"),
+    css: "./components/home/assets/home.css",
+    controller: () => import("./components/home/Controller/home.js"),
+  },
+  "/chat": {
+    title: "Chat",
+    component: () => import("./components/chat/View/chat.js"),
+    css: "./components/chat/assets/chat.css",
+    controller: () => import("./components/chat/Controller/chat.js"),
+  },
+  "/tournaments": {
+    title: "Tournaments",
+    component: () => import("./components/tournaments/View/Tournaments.js"),
+    css: "./components/tournaments/assets/Tournaments.css",
+    controller: () =>
+      import("./components/tournaments/Controller/Tournaments.js"),
+  },
+  "/tournaments/tournament": {
+    title: "Tournament",
+    component: () => import("/components/tournament/View/Tournament.js"),
+    css: "/components/tournament/assets/Tournament.css",
+    controller: () => import("/components/tournament/Controller/Tournament.js"),
+  },
+  "/ranking": {
+    title: "Ranking",
+    component: () => import("./components/Ranking/View/Ranking.js"),
+    css: "./components/Ranking/assets/Ranking.css",
+    controller: () => import("./components/Ranking/Controller/Ranking.js"),
+  },
+  "/sign_in": {
+    title: "Sign In",
+    component: () => import("./components/auth/View/sing_in.js"),
+    css: "./components/auth/assets/auth.css",
+    controller: () => import("./components/auth/Controller/sing_in.js"),
+  },
+  "/sign_in_2fa": {
+    title: "Two-Factor Authentication",
+    component: () => import("./components/auth/View/totp.js"),
+    css: "./components/auth/assets/auth.css",
+    controller: () => import("./components/auth/Controller/totp.js"),
+  },
+  "/sign_up": {
+    title: "Sign Up",
+    component: () => import("./components/auth/View/sign_up.js"),
+    css: "./components/auth/assets/auth.css",
+    controller: () => import("./components/auth/Controller/sign_up.js"),
+  },
+  "/sign_up_email": {
+    title: "Email Verification",
+    component: () => import("./components/auth/View/email_ver.js"),
+    css: "./components/auth/assets/auth.css",
+    controller: () => import("./components/auth/Controller/email_ver.js"),
+  },
+  "/sign_up_info": {
+    title: "Your Information",
+    component: () => import("./components/auth/View/use_info.js"),
+    css: "./components/auth/assets/auth.css",
+    controller: () => import("./components/auth/Controller/use_info.js"),
+  },
+  "/game_match_making": {
+    title: "Match Making",
+    component: () =>
+      import("./components/game/match_making/View/match_making.js"),
+    css: "./components/game/match_making/assets/match_making.css",
+    controller: () =>
+      import("./components/game/match_making/Controller/match_making.js"),
+  },
+  "/game": {
+    title: "In Game",
+    component: () => import("./components/game/in_game/View/game.js"),
+    css: "./components/game/in_game/assets/game.css",
+    controller: () => import("./components/game/in_game/Controller/game.js"),
+  },
+  "/settings": {
+    title: "Settings",
+    component: () => import("./components/settings/View/Settings.js"),
+    css: "./components/settings/assets/style.css",
+    controller: () => import("./components/settings/Controller/Settings.js"),
+  },
+  "/profile": {
+    title: "profile",
+    component: () => import("./components/profile/view/index.js"),
+    css: "./components/profile/assets/style.css",
+    controller: () => import("./components/profile/controller/index.js"),
+  },
+  "*": {
+    title: "404 Not Found",
+    component: () => import("./lib/NotFound/NotFound.js"),
+    css: "./lib/NotFound/notFound.css",
+  },
 };
 
 export default routes;
