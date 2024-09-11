2d8e8f93c7967e13954563e3de7bae4e6a35a589
mac <mac@MacBook-Pro-de-mac-4.local>
2024-09-11 18:03:12 +0100

    stil design the screen

--- frontend/src/components/game/choice_game/Controller/choice_game.js
+++ frontend/src/components/game/choice_game/Controller/choice_game.js
@@ -1,6 +1,32 @@
 const params = new URLSearchParams(window.location.search);
+import { setTournaments } from "/src/components/tournaments/Controller/Tournaments.js";
+import OnlinePlayers from "/src/components/home/components/Controller/OnlinePlayers.js";
+
+
 const mode = params.get("mode");
 
 export default function () {
   console.log("choice game Type here ");
+  const head = document.head;
+
+	const cssFiles = [
+		"/src/components/tournaments/assets/Tournaments.css",
+		"/src/components/home/components/assets/components.css",
+	];
+
+	function loadCSS(href, onLoad, onError) {
+		const link = document.createElement("link");
+		link.rel = "stylesheet";
+		link.href = href;
+		link.onload = onLoad;
+		link.onerror = onError;
+		head.appendChild(link);
+	}
+	cssFiles.forEach((cssFile) => {
+		loadCSS(
+			cssFile,
+		);
+	});
+	setTournaments(3);
+	OnlinePlayers();
 }
